a0fa56e507c99539b740a20dba72e4ce64d72c39
quintenbohte <quintenbohte@gmail.com>
2023-09-14 09:46:50 +0200

    create session

---
+++ dim_discount.py
@@ -0,0 +1,12 @@
+from snowflake.snowpark import Session
+from snowflake.snowpark import DataFrame
+from snowflake.snowpark import functions as fc
+from snowflake.snowpark import Column as cl
+from snowflake.snowpark import Table
+from snowflake.snowpark import Row
+from snowflake.snowpark import DataFrameWriter
+from snowflake.snowpark import types as tp
+import pandas as pd
+import numpy as np
+import os
+
