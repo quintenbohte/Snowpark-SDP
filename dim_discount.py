6979b4a30926dc5bce3e555ccc91a5f7d05248c0
quintenbohte <quintenbohte@gmail.com>
2023-09-14 11:24:14 +0200

    Transformation Discount

--- dim_discount.py
+++ dim_discount.py
@@ -1,3 +1,5 @@
+#%%
+
 from snowflake.snowpark import Session
 from snowflake.snowpark import DataFrame
 from snowflake.snowpark import functions as fc
@@ -9,4 +11,47 @@ from snowflake.snowpark import types as tp
 import pandas as pd
 import numpy as np
 import os
+
+#%%
+
+def create_session():
+
+    connection_parameters = {
+    "account": "RQ70182.eu-central-1",
+    "user": "Quintenbohte",
+    "password": "6HtsyM@12345",
+    "role": "sysadmin",  # optional
+    "warehouse": "COMPUTE_WH",  # optional
+    "database": "QUINTEN_DATABASE",  # optional
+    "schema": "BRONZE",  # optional
+    }
+
+    New_Session = Session.builder.configs(connection_parameters).create()
+
+    return New_Session
+
+session = create_session()
+#%%
+
+def Transformations():
+
+    konp = session.table("STAGING.KONP")
+
+    konp_df = konp[['INDEX', 'KONWA', 'BEGDA', 'KNUMA', 'KSTBW', 'SKTOF', 'ENDDA', 'KSCHL', 'KSLVL', 'VATO', 'VAFR']]
+
+    konp_df = konp_df.fillna({"INDEX": -99, "BEGDA": '9999-99-99'})
+
+    return konp_df
+
+def Write_df_to_SF(df, Table_Name):
+    df.write.mode("overwrite").save_as_table(Table_Name)
+
+#%%
+
+
+Table_Name = 'Discount'
+df = Transformations()
+
+
+Write_df_to_SF(df, Table_Name)
 
